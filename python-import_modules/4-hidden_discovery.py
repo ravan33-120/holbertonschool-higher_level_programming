c221984dd84b4427723d62c9f5d3d258bea1557d
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-19 17:16:58 +0000

    gizli fayl

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import marshal
 
-if **name** == "**main**":
+if __name__ == "__main__":
 with open("/tmp/hidden_4.pyc", "rb") as f:
-f.read(16)  # header-i keçirik
+f.read(16) # fayl headerini keçirik
 code = marshal.load(f)
 
-```
 for name in sorted(n for n in code.co_names if not n.startswith("__")):
     print(name)
-```

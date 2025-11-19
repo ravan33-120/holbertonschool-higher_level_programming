bd031dccbd8b565c865d025d125bd9fb45a0ee1e
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-19 15:57:49 +0000

    args taski

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if **name** == "**main**":
+argc = len(argv) - 1  # arqumentlərin sayı (argv[0] skriptin adı olduğu üçün çıxır)
+if argc == 0:
+print("0 arguments.")
+else:
+if argc == 1:
+print("1 argument:")
+else:
+print(f"{argc} arguments:")
+for i, arg in enumerate(argv[1:], 1):
+print(f"{i}: {arg}")
+

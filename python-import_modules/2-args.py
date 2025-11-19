c91eed6094f40cda6875befe9aa9f7a0dd0c9dd1
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-19 16:27:04 +0000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 from sys import argv
 
 if __name__ == "__main__":
-argc = len(argv) - 1  # arqumentlərin sayı
-if argc == 0:
-print("0 arguments.")
-else:
-if argc == 1:
-print("1 argument:")
-else:
-print(f"{argc} arguments:")
-for i, arg in enumerate(argv[1:], 1):
-print(f"{i}: {arg}")
+    argc = len(argv) - 1  # arqumentlərin sayı
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        if argc == 1:
+            print("1 argument:")
+        else:
+            print(f"{argc} arguments:")
+        for i, arg in enumerate(argv[1:], 1):
+            print(f"{i}: {arg}")
+

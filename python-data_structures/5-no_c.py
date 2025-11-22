3cb0eb3cc9be1cf189ae0c323de906f73cf62190
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-22 14:33:33 +0000

    no C

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
 def no_c(my_string):
-
-    new_string = ''    
-
+    # Yeni boş string yaradırıq
+    new_string = ''
+    
+    # Hər simvolu yoxlayırıq
     for char in my_string:
-
         if char != 'c' and char != 'C':
-
-            new_string += char  
-
+            new_string += char  # c və C olmayan simvolları əlavə edirik
+    
     return new_string

3f7b3f99d3ac412ca49cea0fcfa708b8a5c8981b
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-27 10:10:45 +0000

    inheritance

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines inherits_from function."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that
+    inherited (directly or indirectly) from a_class.
+    Otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class

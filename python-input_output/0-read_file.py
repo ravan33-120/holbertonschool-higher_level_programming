96a9ea15316a40d8c7af0b1557d213d9d2c53dfa
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-28 09:57:51 +0000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 def read_file(filename=""):
     """Read a UTF-8 text file and print its contents to stdout."""
     with open(filename, encoding="utf-8") as f:

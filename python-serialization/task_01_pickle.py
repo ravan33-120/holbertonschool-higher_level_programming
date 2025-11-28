b778689fb04ab2a273aca931492d4865d7716fe1
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-28 16:34:48 +0000

    task_01_pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# task_01_pickle.py
+
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Print the object's attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the object and save it to a file."""
+        try:
+            with open(filename, "wb") as file:
+                pickle.dump(self, file)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize the object from a file and return it."""
+        try:
+            with open(filename, "rb") as file:
+                obj = pickle.load(file)
+                return obj
+        except Exception:
+            return None

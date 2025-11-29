83833466288bde2f1481563bdf38511448ca34e6
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 19:19:03 +0000

    user2 yarat ve yalniz select icazesi

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,3 @@
 -- user 2
-CREATE USER IF NOT EXISTS user_0d_2 ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2@'localhost';

f680c39b4a8d335c800f899929ce749530539403
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 17:56:03 +0000

    user1 ve user2-ye butun icazeler

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- all privilages to user1 and user2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';

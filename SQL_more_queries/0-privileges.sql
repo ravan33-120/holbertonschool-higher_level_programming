a54f3b9046df57cd49f27e8473f0d977cdb70925
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 17:59:05 +0000

    user1 ve user2-nin icazeleri

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- all privilages to user1 and user2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- user 1 ve user 2 icazeleri
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

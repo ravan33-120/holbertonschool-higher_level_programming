9aaebcbb9523d528d44f5ec21ffb51f34431a83f
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 14:11:17 +0000

    second table ve rowlarin yaratmaq

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- create second-table and rows
 CREATE TABLE IF NOT EXISTS second-table (
-    id INT
-    name VARCHAR(256)
+    id INT,
+    name VARCHAR(256),
     score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);

a0614d5a0b1f33d7bc375f3ec41c0a97a563b0ef
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 20:17:29 +0000

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- cities 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	state_id INT NOT NULL,
-FOREIGN KEY (state_id) REFERENCES states(id),
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL );

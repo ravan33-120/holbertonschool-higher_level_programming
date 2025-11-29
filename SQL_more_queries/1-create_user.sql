746bc4943cadf433782ace6a57fc8ac441b44aa2
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-29 18:55:56 +0000

    user yarat ve butun icazeler ver

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- user1 yaradib butun icazeler veririk
-CREATE USER 'user_0d_1`@`localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

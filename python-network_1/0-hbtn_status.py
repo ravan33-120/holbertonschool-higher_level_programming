be3b8060c4cc5e3516834c39f22dadab7d553222
ravan33-120 <ravannabiyev466@gmail.com>
2025-12-02 13:53:27 +0000

    holberton status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from urllib import request
 
-url = "https://intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
 
-with request.urlopen(url) as response:
-    body = response.read()
+    with request.urlopen(url) as response:
+        body = response.read()
 
-print("Body response:")
-print("    - type: {}".format(type(body)))
-print("    - content: {}".format(body))
-print("    - utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode("utf-8")))
